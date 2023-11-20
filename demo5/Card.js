177c65afee2a1428e11a99001a3b87029dde73db
Payal Bansal <jahanvi.bansal@gmail.com>
2023-11-20 12:10:41 +0530

    Add files via upload

---
+++ demo5/Card.js
@@ -0,0 +1,8 @@
+import React from "react"
+export const Card = (props)=> {
+    return (
+        <div className={props.name}>
+            <h1>Hello</h1>
+        </div>
+    )
+}
---
+++ demo5/test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card } from "./Card";
+import styles from './style.module.css'
+function  Test() { 
+
+   
+
+    return (
+  <ul>
+    <div>
+        <Card name = {styles.Failstyle}/>
+    </div>
+    </ul>
+        )
+}
+
+export default Test
